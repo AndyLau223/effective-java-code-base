7251824a4889eaad4e63f1f01343008e72f53bd5
Andy <jiancong.liu@outlook.com>
2022-09-28 23:41:38 +0800

    Add telescoping constructor

---
+++ src/creating/and/destroying/objects/items/telescopingconstructor/NutritionFacts.java
@@ -0,0 +1,55 @@
+package creating.and.destroying.objects.items.telescopingconstructor;
+
+/**
+ * Telescoping constructor pattern - does not scale well, this is anti-pattern!
+ *
+ * What's the project with that?
+ * The problem with this (anti)pattern is that once your class has to much constructors
+ * and constructors are 4 or more parameters long it becomes difficult to
+ * remember the required order of the parameters as well as what particular
+ * constructor you might want in a given situation.
+ * Giving yourself and your fellow developers a hard time trying to initialize that class.
+ *
+ */
+public class NutritionFacts {
+    private final int servingSize;  // (mL)            required
+    private final int servings;     // (per container) required
+    private final int calories;     // (per serving)   optional
+    private final int fat;          // (g/serving)     optional
+    private final int sodium;       // (mg/serving)    optional
+    private final int carbohydrate; // (g/serving)     optional
+
+    public NutritionFacts(int servingSize, int servings) {
+        this(servingSize, servings, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings,
+                          int calories) {
+        this(servingSize, servings, calories, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings,
+                          int calories, int fat) {
+        this(servingSize, servings, calories, fat, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings,
+                          int calories, int fat, int sodium) {
+        this(servingSize, servings, calories, fat, sodium, 0);
+    }
+    public NutritionFacts(int servingSize, int servings,
+                          int calories, int fat, int sodium, int carbohydrate) {
+        this.servingSize  = servingSize;
+        this.servings     = servings;
+        this.calories     = calories;
+        this.fat          = fat;
+        this.sodium       = sodium;
+        this.carbohydrate = carbohydrate;
+    }
+
+    public static void main(String[] args) {
+        NutritionFacts cocaCola =
+                new NutritionFacts(240, 8, 100, 0, 35, 27);
+    }
+
+}
